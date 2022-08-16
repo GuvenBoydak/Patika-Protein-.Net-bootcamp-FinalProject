7cf722a6a05e5504e6cd32b8bc42646fd969dcd1
güven boydak <gvn.boydak@gmail.com>
2022-08-16 17:27:50 +0300

    FileHelper eklendi. Product add metodu refaktor edildi.

--- FinalProject.Api/Controllers/ProductsController.cs
+++ FinalProject.Api/Controllers/ProductsController.cs
@@ -3,7 +3,9 @@ using FinalProject.Base;
 using FinalProject.Business;
 using FinalProject.DTO;
 using FinalProject.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace FinalProject.Api
 {
@@ -13,11 +15,15 @@ namespace FinalProject.Api
     {
         private readonly IProductService _productService;
         private readonly IMapper _mapper;
+        private readonly IFileHelper _fileHelper;
+        private readonly IConfiguration _configuration;
 
-        public ProductsController(IProductService productService, IMapper mapper)
+        public ProductsController(IProductService productService, IMapper mapper, IFileHelper fileHelper, IConfiguration configuration)
         {
             _productService = productService;
             _mapper = mapper;
+            _fileHelper = fileHelper;
+            _configuration = configuration;
         }
 
 
@@ -61,25 +67,38 @@ namespace FinalProject.Api
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto, $"{id} numarali ürün listelendi"));
         }
 
+        [Authorize]
         [HttpPost]
-        public IActionResult Add([FromBody] ProductAddDto productAddDto)
+        public IActionResult Add([FromForm(Name = "Image")] IFormFile file,/*[FromForm]*/ ProductAddDto productAddDto)//Postman de çalişiyor 
         {
-            Product product=_mapper.Map<Product>(productAddDto);
+            if(file==null)//Girelen resim vamrı bakıyoruz.
+                throw new Exception("REsim kısmı boş geçilemez");
+
+            Product product = _mapper.Map<Product>(productAddDto);
+
+            product.AppUserID = int.Parse((User.Identity as ClaimsIdentity).FindFirst("AppUserID").Value);//Kullanıcı claimlerinden Id sini aldık.
+
+            product.ImageUrl = _fileHelper.Add(file,_configuration.GetSection("ImageUrl").Value);//Resim ekleme işlemi
 
             _productService.Add(product);
 
-            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204,"Ekleme işlemi Başarılı"));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204, "Ekleme işlemi Başarılı"));
         }
 
+
         [HttpPut]
-        public async Task<IActionResult> Update([FromBody]ProductUpdateDto productUpdateDto)
+        public async Task<IActionResult> Update(/*[FromForm]*/ProductUpdateDto productUpdateDto,[FromForm(Name = "Image")] IFormFile file)//Postman de Çalişiyor.
         {
             Product product = _mapper.Map<Product>(productUpdateDto);
+
+            if (productUpdateDto.ImageUrl != null)//Gonderilen mmodellin ImageUrl boş degilse resim günceliyoruz.
+                _fileHelper.Update(file, _configuration.GetSection("ImageUrl").Value + product.ImageUrl, _configuration.GetSection("ImageUrl").Value);
 
             await _productService.UpdateAsync(product);
 
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204, "Güncelleme İşlemi başarılı"));
         }
+
 
         [HttpDelete("{id}")]
         public IActionResult Delete([FromRoute]int id)
---
+++ FinalProject.Base/FileHelper/IFileHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FinalProject.Base
+{
+
+    public interface IFileHelper
+    {
+        string Add(IFormFile file, string root);
+
+        void Delete(string filePath);
+
+        void Update(IFormFile file, string filePath, string root);
+    }
+
+}
